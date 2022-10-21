36a45bfd8a94edcbad1601a4308d0e3d273473bf
chystarr <chynnastarr2@gmail.com>
2022-10-21 12:00:00 -0400

    Made static ZipSearchField component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
+function ZipSearchField(props) {
+  const handleChange = () => {
+    console.log("Input changed!");
+  }
+  return (
+    <div>
+      <form>
+        <label>Zip Code:</label>
+        <br/>
+        <input onChange={handleChange}></input>
+      </form>
+    </div>
+  );
+}
 
 function City(props) {
   return <div>This is the City component</div>;
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
-}
-
 function App() {
+  const [zipSearch, setZipSearch] = useState("")
+  const [foundCities, setFoundCities] = useState([])
+
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
+        <ZipSearchField/>
         <div>
           <City />
           <City />
